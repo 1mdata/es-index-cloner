06db96b5783294d1dc8dd2ebabbab3f6693ccdc1
冯朋斌 <fengpb@1mdata.com>
2016-12-02 13:47:23 +0800

    当 target index exists 时，通过比较 文档 version 确定是否同步

--- index_cloner.py
+++ index_cloner.py
@@ -4,8 +4,8 @@
 import argparse
 from progress.bar import ShadyBar
 from elasticsearch import Elasticsearch,connection as es_connection
-from elasticsearch.helpers import streaming_bulk
-
+from elasticsearch.helpers import streaming_bulk,BulkIndexError
+from elasticsearch.exceptions import *
 
 class IndexCloner(object):
     def __init__(self, source_index, target_index, source_es_ip_port,target_es_ip_port, shard_count, replica_count,bulk_size):
@@ -40,7 +40,10 @@ class IndexCloner(object):
                  "settings": {"index": {"number_of_shards": self.shard_count, "number_of_replicas": self.replica_count}}
                 ,"mappings": source_mappings['mappings']}
         # Create index with settings and mappings
-        self.target_es.indices.create(index=self.target_index,body=index_settings_mappings)
+        exists = self.target_es.indices.exists(index=self.target_index,ignore=[404])
+        self.target_index_is_old = exists
+        if exists == False:
+            self.target_es.indices.create(index=self.target_index,body=index_settings_mappings)
         
 
     def _get_mappings(self):
@@ -53,6 +56,7 @@ class IndexCloner(object):
         for hit in hits:
             doc_id = hit['_id']
             doc_type = hit['_type']
+            doc_version = hit['_version']
             doc_parent = hit.get('_parent')
             doc_routing = hit.get('_routing')
             
@@ -62,18 +66,36 @@ class IndexCloner(object):
                 "_id": doc_id,
                 "_source": hit['_source']
             }
-            
+            target_get_doc_kw = {}
             if doc_parent:
                 document_action["_parent"] = doc_parent
+                target_get_doc_kw['parent'] = doc_parent
             
             if doc_routing:
                 document_action["_routing"] = doc_routing
+                target_get_doc_kw['routing'] = doc_routing
             
-            yield document_action
+            modified = True
+            if self.target_index_is_old:
+                " check document "
+                try:
+                    target_document = self.target_es.get(index=self.target_index,doc_type=doc_type,id=doc_id,ignore=[404],**target_get_doc_kw)
+                    found = target_document['found']
+                    if(found==True):
+                        target_version = target_document.get('_version')
+                        # check target version >= source version
+                        if (target_version >= doc_version):
+                            modified = False
+                        # print(target_document)
+                except TransportError as err:
+                    pass
+            # new or modified    
+            if modified:
+                yield document_action
 
 
     def _copy_data(self):
-        scroll = self.source_es.search(index=self.source_index,scroll='1m',search_type='scan',size=self.bulk_size,timeout='60s')
+        scroll = self.source_es.search(index=self.source_index,scroll='1m',search_type='scan',size=self.bulk_size,version=True,timeout='60s')
         sid = scroll['_scroll_id']
         total_size = scroll['hits']['total']
         hits_size = total_size
@@ -89,17 +111,22 @@ class IndexCloner(object):
             actions = self._bulk_hits(hits)
             kw = {}
             kw['timeout'] = '60s'
-            res = streaming_bulk(client=self.target_es,actions=actions,**kw)
+            res = []
+            try:
+                res = streaming_bulk(client=self.target_es,actions=actions,**kw)
+            except BulkIndexError as err:
+                print(err)
+                pass
+           
             for ok,re in res:
                 if not ok:
                     print(re)
-
             # refresh index
             self.target_es.indices.refresh(index=self.target_index)
             # dealt size
             dealt_size += hits_size
             bar.goto(dealt_size)
-
+        print('\nDone !')
         # clear scroll 
         # self.source_es.clear_scroll(scroll_id=sid,body=str(sid))
 
